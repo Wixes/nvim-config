edefdb1ea63bc0a64dda75c4a3ec6da65779c585
Dmitry <dmitri.koma@yandex.ru>
2025-08-21 13:32:39 +0700

    Added plugins for git

--- init.lua
+++ init.lua
@@ -999,6 +999,8 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  { import = 'custom.plugins.neogit' },
+  { import = 'custom.plugins.diffview' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
